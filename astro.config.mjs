29396ff6d70588ce48846243067ef64ce14a8e3f
mbrueggemeyer-design <m.brueggemeyer@googlemail.com>
2026-08-23 22:35:35 +0200

    Update astro.config.mjs
    indizierungsproblem beheben

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
 import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://maikbrueggemeyer.de',
+  trailingSlash: 'always',
   integrations: [sitemap()],
 });
